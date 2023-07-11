708f8a8e6bb72e622ada4c40a68d90a8ec48222e
Truong Giang <giangntse150747@gmail.com>
2023-07-12 03:54:15 +0700

    add event action

---
+++ src/app/services/participate.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
+import { Observable, map } from 'rxjs';
+import { PaginationResponse } from '../models/paging.model';
+import { NoDataResponse } from '../models/auth.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ParticipateService {
+  private apiUrl: string = environment.apiUrl;
+  BASE_URL = `${this.apiUrl}/participant`;
+  //GET_OWN_POST_URL = `${this.apiUrl}/participant`;
+  CREATE_POST_URL = `${this.apiUrl}/participant`;
+
+  constructor(private httpClient: HttpClient, private toastr: ToastrService) {}
+
+  joinEvent(event_id: string): Observable<any> {
+    const body = {
+      event_id: event_id,
+    };
+
+    return this.httpClient.post<NoDataResponse>(this.BASE_URL, body).pipe(
+      map((response: NoDataResponse) => {
+        return response;
+      })
+    );
+  }
+}
